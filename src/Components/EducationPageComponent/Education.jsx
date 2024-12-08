bc00a6483133f62763a406fe5c865b3ac6a308a6
vikas9872 <shanabhogvikas648.com>
2024-12-08 13:44:47 +0530

    Education shadow updated

--- src/Components/EducationPageComponent/Education.jsx
+++ src/Components/EducationPageComponent/Education.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useEffect } from 'react';
 import education from '../EducationList/education'
+import AOS from 'aos';
+import 'aos/dist/aos.css'
 const EducationPage = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []); 
   return (
     <div className='flex flex-col pt-10 items-center h-auto'>
       <div className='text-black dark:text-white items-center font-bold text-6xl md:text-7xl font-roboto-condensed p-10' data-aos="fade-right">
