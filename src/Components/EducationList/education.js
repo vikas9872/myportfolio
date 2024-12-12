6bc277f7fc1f3341f5111a8f4147953191b1c1a8
vikas9872 <shanabhogvikas648.com>
2024-12-12 06:54:09 +0530

    "updated course"

--- src/Components/EducationList/education.js
+++ src/Components/EducationList/education.js
@@ -2,24 +2,28 @@ const education=[
     {
         id: 1,
         name: "J. S. S. Public School",
+        course: "X",
         yoc: "2017",
         school: "./ImageAssets/jssschool.jpg"
     },
     {
         id: 2,
         name: "Sri Siddaganga PU College",
+        course: "XII",
         yoc: "2019",
         school: "./ImageAssets/siddagangapucollege.jpg"
     },
     {
         id: 3,
         name: "Presidency University",
+        course: "B.Tech in Computer Science Technology",
         yoc: "2023",
         school: "./ImageAssets/presidencyuniversity.jpg"
     },
     {
         id: 4,
         name: "Global Academy of Technology",
+        course: "M.Tech in Computer Science Technology",
         yoc: "2025",
         school: "./ImageAssets/globalacademyoftechnology.jpg"
     }
