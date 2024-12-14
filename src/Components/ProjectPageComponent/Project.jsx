8bda77e438e6f9e5dfe8745c7220752005830430
vikas9872 <shanabhogvikas648.com>
2024-12-14 18:56:18 +0530

    Final Commit

--- src/Components/ProjectPageComponent/Project.jsx
+++ src/Components/ProjectPageComponent/Project.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Cards from './ProjectsCards'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 const ProjectPage = () => {
+    useEffect(() => {
+      AOS.init({ duration: 1000 });
+    }, []);
   return (
     <div className='flex flex-col pt-10 items-center h-auto'>
       <div className='text-black dark:text-white items-center font-bold text-6xl md:text-7xl font-roboto-condensed p-10' data-aos="fade-right">
